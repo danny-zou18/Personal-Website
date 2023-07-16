3811560e201e30767ae89b634686d279cb28badd
danny-zou18 <dzou3814@gmail.com>
2023-07-16 16:40:17 -0400

    new file:   base/__init__.py 	new file:   base/__pycache__/__init__.cpython-310.pyc 	new file:   base/__pycache__/admin.cpython-310.pyc 	new file:   base/__pycache__/apps.cpython-310.pyc 	new file:   base/__pycache__/models.cpython-310.pyc 	new file:   base/__pycache__/urls.cpython-310.pyc 	new file:   base/__pycache__/views.cpython-310.pyc 	new file:   base/admin.py 	new file:   base/apps.py 	new file:   base/migrations/__init__.py 	new file:   base/migrations/__pycache__/__init__.cpython-310.pyc 	new file:   base/models.py 	new file:   base/templates/home.html 	new file:   base/templates/main.html 	new file:   base/templates/navbar.html 	new file:   base/templates/post.html 	new file:   base/templates/posts.html 	new file:   base/templates/profile.html 	new file:   base/tests.py 	new file:   base/urls.py 	new file:   base/views.py 	new file:   db.sqlite3 	new file:   manage.py 	new file:   personal_website/__init__.py 	new file:   personal_website/__pycache__/__init__.cpython-310.pyc 	new file:   personal_website/__pycache__/settings.cpython-310.pyc 	new file:   personal_website/__pycache__/urls.cpython-310.pyc 	new file:   personal_website/__pycache__/wsgi.cpython-310.pyc 	new file:   personal_website/asgi.py 	new file:   personal_website/settings.py 	new file:   personal_website/urls.py 	new file:   personal_website/wsgi.py

---
+++ base/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def posts(request):
+    return render(request, 'posts.html')
+
+def post(request):
+    return render(request, 'post.html')
+
+def profile(request):
+    return render(request, 'profile.html')
