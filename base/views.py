6c67ce524a110dbfab936116d5708d41c003bd74
dannyzou18 <dzou3814@gmail.com>
2023-07-30 18:41:36 -0400

    contact-home done 	modified:   base/__pycache__/admin.cpython-311.pyc 	modified:   base/__pycache__/models.cpython-311.pyc 	modified:   base/__pycache__/views.cpython-311.pyc 	modified:   base/admin.py 	new file:   base/migrations/0001_initial.py 	new file:   base/migrations/__pycache__/0001_initial.cpython-311.pyc 	modified:   base/models.py 	modified:   base/templates/base/about.html 	modified:   base/templates/base/contact.html 	modified:   base/templates/base/index.html 	modified:   base/templates/base/navbar.html 	modified:   base/templates/base/portfolio.html 	modified:   base/views.py 	modified:   db.sqlite3 	new file:   static/css/about-style.css 	new file:   static/css/contact-style.css 	renamed:    static/css/home.css -> static/css/home-style.css 	new file:   static/css/nav-style.css 	new file:   static/css/portfolio-style.css

--- base/views.py
+++ base/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Contact
+from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
@@ -11,6 +13,20 @@ def socials(request):
     return render(request, 'base/socials.html')
 
 def contact(request):
+    if request.method=="POST":
+        contact = Contact()
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        subject=request.POST.get('subject')
+        message=request.POST.get('message')
+
+        contact.name=name
+        contact.email=email
+        contact.subject=subject
+        contact.message=message
+
+        contact.save()
+        return HttpResponse("<h1>Thanks for reaching out!</h1>")
     return render(request, "base/contact.html")
 
 def portfolio(request):
